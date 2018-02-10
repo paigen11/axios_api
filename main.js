bef983e5993e8f7b77905f390c3e8072c83c43f5
Paige Niedringhaus <paige_e_niedringhaus@homedepot.com>
2018-02-10 17:43:25 -0500

    clean up

--- main.js
+++ main.js
@@ -12,29 +12,6 @@ function performGetRequest1() {
         })
 }
 
-function generateSuccessHTMLOutput(response) {
-    return  '<h4>Result</h4>' +
-        '<h5>Status:</h5> ' +
-        '<pre>' + response.status + ' ' + response.statusText + '</pre>' +
-        '<h5>Headers:</h5>' +
-        '<pre>' + JSON.stringify(response.headers, null, '\t') + '</pre>' +
-        '<h5>Data:</h5>' +
-        '<pre>' + JSON.stringify(response.data, null, '\t') + '</pre>';
-}
-
-function generateErrorHTMLOutput(error) {
-    return  '<h4>Result</h4>' +
-        '<h5>Message:</h5> ' +
-        '<pre>' + error.message + '</pre>' +
-        '<h5>Status:</h5> ' +
-        '<pre>' + error.response.status + ' ' + error.response.statusText + '</pre>' +
-        '<h5>Headers:</h5>' +
-        '<pre>' + JSON.stringify(error.response.headers, null, '\t') + '</pre>' +
-        '<h5>Data:</h5>' +
-        '<pre>' + JSON.stringify(error.response.data, null, '\t') + '</pre>';
-
-}
-
 //GET request with specified id param
 function performGetRequest2() {
     var resultElement = document.getElementById('getResult2');
@@ -46,13 +23,13 @@ function performGetRequest2() {
             id: todoId
         }
     })
-    .then(function (response){
-        console.log(response);
-        resultElement.innerHTML = generateSuccessHTMLOutput(response);
-    })
-    .catch(function (error){
-        resultElement.innerHTML = generateErrorHTMLOutput(error);
-    })
+        .then(function (response){
+            console.log(response);
+            resultElement.innerHTML = generateSuccessHTMLOutput(response);
+        })
+        .catch(function (error){
+            resultElement.innerHTML = generateErrorHTMLOutput(error);
+        })
 }
 
 //POST request
@@ -76,5 +53,27 @@ function performPostRequest(e){
         .catch(function (error){
             resultElement.innerHTML = generateErrorHTMLOutput(error);
         })
+}
+
+function generateSuccessHTMLOutput(response) {
+    return  '<h4>Result</h4>' +
+        '<h5>Status:</h5> ' +
+        '<pre>' + response.status + ' ' + response.statusText + '</pre>' +
+        '<h5>Headers:</h5>' +
+        '<pre>' + JSON.stringify(response.headers, null, '\t') + '</pre>' +
+        '<h5>Data:</h5>' +
+        '<pre>' + JSON.stringify(response.data, null, '\t') + '</pre>';
+}
+
+function generateErrorHTMLOutput(error) {
+    return  '<h4>Result</h4>' +
+        '<h5>Message:</h5> ' +
+        '<pre>' + error.message + '</pre>' +
+        '<h5>Status:</h5> ' +
+        '<pre>' + error.response.status + ' ' + error.response.statusText + '</pre>' +
+        '<h5>Headers:</h5>' +
+        '<pre>' + JSON.stringify(error.response.headers, null, '\t') + '</pre>' +
+        '<h5>Data:</h5>' +
+        '<pre>' + JSON.stringify(error.response.data, null, '\t') + '</pre>';
 
 }
